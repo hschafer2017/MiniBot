9bbae14dca2f4721b5ffe44f14a5a10dd7bb8af2
hschafer2017 <hschafer@umich.edu>
2019-12-08 10:49:24 +0000

    added basic custom training data to chatbot

--- minibot.py
+++ minibot.py
@@ -5,17 +5,21 @@ from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 chatbot = ChatBot("MiniBot",
                   storage_adapter='chatterbot.storage.SQLStorageAdapter',
                   logic_adapters=[
-                      'chatterbot.logic.MathematicalEvaluation',
-                      'chatterbot.logic.TimeLogicAdapter',
-                      'chatterbot.logic.BestMatch'
-                  ],
+                    "chatterbot.logic.MathematicalEvaluation",
+                    "chatterbot.logic.TimeLogicAdapter",
+                    "chatterbot.logic.BestMatch"
+                    ],
+                  input_adapter="chatterbot.input.TerminalAdapter",
+                  output_adapter="chatterbot.output.TerminalAdapter",
                   database_uri='sqlite:///database.sqlite3'
 )
+
+
 general_trainer = ChatterBotCorpusTrainer(chatbot)
 
 general_trainer.train(
-    "chatterbot.corpus.english.greetings",
-    "chatterbot.corpus.english.conversations",
+    "chatterbot.corpus.english", 
+    "training_data/training_data.yml"
 )
 
 while True:
